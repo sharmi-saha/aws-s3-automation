6e7e13d69deb17dd478629d4b18410b61f87ee81
sharmi-saha-exa <sharmi.saha@exabeam.com>
2024-01-23 17:47:04 +0530

    test

--- data/auth_key.py
+++ data/auth_key.py
@@ -1,2 +1,4 @@
+# these are dummy key secrets. replace with valid AWS access key & secrets (base64(utf8) encrypted)
+
 AWS_ACCESS_KEY = b'ZHVtbXlBY2Nlc3NLZXk='
 AWS_SECRET_KEY = b'ZHVtbXlTZWNyZXRLZXk='
